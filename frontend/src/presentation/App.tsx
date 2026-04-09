ff33b02ad9000321a66883a69275e9952cb6e2ea
Victor Javier Vivas <victorjaviervivas@gmail.com>
2026-04-09 13:29:13 -0400

    feat(frontend): configure oxlint, eslint and vscode workspace standards

--- frontend/src/presentation/App.tsx
+++ frontend/src/presentation/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import '@/presentation/App'
 
 function App() {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from 'url'
 import path from 'path'
+// Simulamos __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
